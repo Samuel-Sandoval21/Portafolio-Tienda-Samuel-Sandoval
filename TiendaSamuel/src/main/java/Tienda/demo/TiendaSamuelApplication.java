079a420ac3b38f513c4fb09d315a300de9a47511
Samuel Sandoval <ssandoval40716@ufide.ac.cr>
2025-09-17 20:58:14 -0600

    commit semana 2
    segundo commit

---
+++ TiendaSamuel/src/main/java/Tienda/demo/TiendaSamuelApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSamuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaSamuelApplication.class, args);
+	}
+
+}
