170d6b0ba2862963b96ee7626c64c0829fb54169
Samuel Sandoval <ssandoval40716@ufide.ac.cr>
2025-10-02 16:45:58 -0600

    Semana 3 (Semana 2)
    Corregir y completar en casa

---
+++ TiendaSamuel/src/main/java/Tienda/demo/repository/CategoriaRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tienda.demo.repository;
+import Tienda.demo.domain.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author HP
+ */
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
+    public List<Categoria> findByActivoTrue();
+    
+}
