a32d30e849bd2ad94636b5b107314cb29e8af82f
Samuel Sandoval <ssandoval40716@ufide.ac.cr>
2025-11-19 23:15:27 -0600

    Commit Semana 11
    Completa en base a PDF y ppt

--- TiendaSamuel/src/main/java/Tienda/demo/repository/UsuarioRepository.java
+++ TiendaSamuel/src/main/java/Tienda/demo/repository/UsuarioRepository.java
@@ -4,9 +4,23 @@
  */
 package Tienda.demo.repository;
 import Tienda.demo.domain.Usuario;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Optional<Usuario> findByUsernameAndActivoTrue(String username);
+
+    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+
+    public List<Usuario> findByActivoTrue();
+
+    public Optional<Usuario> findByUsername(String username);
+
+    public Optional<Usuario> findByUsernameAndPassword(String username, String Password);
+
+    public Optional<Usuario> findByUsernameOrCorreo(String username, String correo);
+
+    public boolean existsByUsernameOrCorreo(String username, String correo);
+
 }
